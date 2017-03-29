842b2d6a4f339b4117b1fa22bbd3a151662a1499
Austin Wood <austin@austinkwood.com>
2017-03-29 10:45:54 -0700

    Add background to canvas

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Cell from './cell.jsx';
+import Board from './board.jsx';
 import { Layer, Stage } from 'react-konva';
 
 const App = () => (
   <Stage width={700} height={700}>
     <Layer>
+      <Board width={700} height={700} />
       <Cell x={10} y={10} />
       <Cell x={150} y={190} />
     </Layer>
---
+++ frontend/components/board.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Rect } from 'react-konva';
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Rect
+        x={0} y={0}
+        width={this.props.width} height={this.props.height}
+        fill={'black'} />
+    );
+  }
+}
+
+export default Board;
