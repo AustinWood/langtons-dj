f7bf05d549da98a94b337ee8c35321bf13f1d7d4
Austin Wood <austin@austinkwood.com>
2017-03-29 11:24:56 -0700

    Pass grid to canvas via props

--- frontend/components/canvas.jsx
+++ frontend/components/canvas.jsx
@@ -3,22 +3,30 @@ import Cell from './cell.jsx';
 import Board from './board.jsx';
 import { Layer, Stage } from 'react-konva';
 
-const cells = () => {
+class Canvas extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-  return (
-    [10, 30, 50, 200].map(pos => (
-      <Cell x={pos} y={pos} />
-    ))
-  );
-};
+  cells(cellsArr) {
+    console.log(cellsArr);
+    return (
+      [10, 30, 50, 200].map(pos => (
+        <Cell x={pos} y={pos} />
+      ))
+    );
+  }
 
-const Canvas = () => (
-  <Stage width={700} height={700}>
-    <Layer>
-      <Board width={700} height={700} />
-      {cells()}
-    </Layer>
-  </Stage>
-);
+  render() {
+    return (
+      <Stage width={700} height={700}>
+        <Layer>
+          <Board width={700} height={700} />
+          {this.cells(this.props.cells)}
+        </Layer>
+      </Stage>
+    );
+  }
+}
 
 export default Canvas;
