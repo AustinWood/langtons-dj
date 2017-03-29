07c7dc4a32a4f7801ca2e3a46add973f706631ea
Austin Wood <austin@austinkwood.com>
2017-03-29 10:52:26 -0700

    Render multiple cells using .map

--- frontend/components/canvas.jsx
+++ frontend/components/canvas.jsx
@@ -3,12 +3,19 @@ import Cell from './cell.jsx';
 import Board from './board.jsx';
 import { Layer, Stage } from 'react-konva';
 
+const cells = () => (
+  [10, 30, 50, 200].map(pos => (
+    <Cell x={pos} y={pos} />
+  ))
+);
+
+
+// <Cell x={150} y={190} />
 const Canvas = () => (
   <Stage width={700} height={700}>
     <Layer>
       <Board width={700} height={700} />
-      <Cell x={10} y={10} />
-      <Cell x={150} y={190} />
+      {cells()}
     </Layer>
   </Stage>
 );
