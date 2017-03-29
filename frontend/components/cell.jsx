2eb0d4faa6890b39d90feaca86b7b4773880a804
Austin Wood <austin@austinkwood.com>
2017-03-29 11:59:55 -0700

    Make cells circles

--- frontend/components/cell.jsx
+++ frontend/components/cell.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Rect } from 'react-konva';
+import { Circle } from 'react-konva';
 
 class Cell extends React.Component {
   constructor(props) {
@@ -14,18 +15,25 @@ class Cell extends React.Component {
   //     color: Konva.Util.getRandomColor()
   //   });
   // }
+  // onClick={this.handleClick} // add to component props
 
   render() {
     return (
-      <Rect
-        x={this.props.x * this.props.cellSize}
-        y={this.props.y * this.props.cellSize}
-        width={this.props.cellSize}
-        height={this.props.cellSize}
-        fill={this.props.color}
-        onClick={this.handleClick} />
+      <Circle
+        x={this.props.x * this.props.cellSize + this.props.cellSize / 2}
+        y={this.props.y * this.props.cellSize + this.props.cellSize / 2}
+        radius={this.props.cellSize * 0.45}
+        fill={this.props.color} />
     );
   }
 }
 
 export default Cell;
+
+// <Rect
+//   x={this.props.x * this.props.cellSize}
+//   y={this.props.y * this.props.cellSize}
+//   width={this.props.cellSize}
+//   height={this.props.cellSize}
+//   fill={this.props.color}
+//   onClick={this.handleClick} />
