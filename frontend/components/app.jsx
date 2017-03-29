1632516222c7f07f67d541850cd65cb68e7e84ae
Austin Wood <austin@austinkwood.com>
2017-03-29 10:47:22 -0700

    Extract Canvas to separate component

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,16 +1,8 @@
 import React from 'react';
-import Cell from './cell.jsx';
-import Board from './board.jsx';
-import { Layer, Stage } from 'react-konva';
+import Canvas from './canvas.jsx';
 
 const App = () => (
-  <Stage width={700} height={700}>
-    <Layer>
-      <Board width={700} height={700} />
-      <Cell x={10} y={10} />
-      <Cell x={150} y={190} />
-    </Layer>
-  </Stage>
+  <Canvas />
 );
 
 export default App;
