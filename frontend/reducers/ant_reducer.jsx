080d2011bd69e1c1ab4ee53d3ea950df8b53c160
Austin Wood <austin@austinkwood.com>
2017-03-30 07:34:01 -0700

    Add ants to state with click

--- frontend/reducers/ant_reducer.jsx
+++ frontend/reducers/ant_reducer.jsx
@@ -1,24 +1,33 @@
 import merge from 'lodash/merge';
 import { TOGGLE_ANT } from '../actions/actions';
 
-const gridSize = 20;
+const generateId = () => {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (let i = 0; i < 5; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+};
 
 // const _ants = Object.freeze({
 //   ants: []
 // });
-const _ants = [];
+const _ants = {};
 
 const AntReducer = (state = _ants, action) => {
   Object.freeze(state);
   let newState = {};
   switch(action.type) {
     case TOGGLE_ANT:
-      console.log(action);
-      // console.log(state.grid.cells[2][0].color);
-
-      newState = merge({}, state);
-      newState.cells[action.y][action.x].color = "red";
-      return state;
+      const id = generateId();
+      newState[id] = {
+        antId: id,
+        x_0: action.x, y_0: action.y, dir_0: 'r',
+        x: action.x, y: action.y, dir: 'r',
+        musical_attrs: null
+      };
+      return merge({}, state, newState);
     default:
       return state;
   }
