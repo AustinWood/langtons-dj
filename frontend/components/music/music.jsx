693027ffbd108b6184cecff850e68afcd978c7a9
Austin Wood <austin@austinkwood.com>
2017-04-03 16:24:26 -0700

    Implement React-Music with arbitrary melody and tie to isPlaying state!

--- frontend/components/music/music.jsx
+++ frontend/components/music/music.jsx
@@ -1,7 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import merge from 'lodash/merge';
-// import flock from 'flocking';
+import React, { Component } from 'react';
 
 import {
   Analyser,
@@ -11,35 +8,120 @@ import {
   Synth,
 } from 'react-music';
 
-class Music extends React.Component {
+import Polysynth from './polysynth';
+import Visualization from './visualization';
+
+// import './music.css';
+
+export default class Demo extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      playing: true,
+      lightMode: true
+    };
+
+    this.handleAudioProcess = this.handleAudioProcess.bind(this);
+    this.handlePlayToggle = this.handlePlayToggle.bind(this);
+    this.toggleLightMode = this.toggleLightMode.bind(this);
   }
-
-  componentDidMount() {
-    // this.handleHandler();
+  handleAudioProcess(analyser) {
+    this.visualization.audioProcess(analyser);
   }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.props.isPlaying) {
-      console.log(this.props);
-      this.playSound();
-    }
+  handlePlayToggle() {
+    this.setState({
+      playing: !this.state.playing,
+    });
   }
-
-  playSound() {
-
+  toggleLightMode(){
+    this.setState({lightMode: !this.state.lightMode});
   }
-
-  ///////////////
-  ///// Render
   render() {
     return (
-      <div id='music'>
-        The music div
+      <div style={this.state.lightMode ? {
+        paddingTop: '30px'
+      } : {
+        backgroundColor: '#000',
+        width: '100%',
+        height: '100%',
+        paddingTop: '30px'
+      }}>
+        <Song
+          playing={this.props.isPlaying}
+          tempo={90}
+        >
+          <Analyser onAudioProcess={this.handleAudioProcess}>
+            <Sequencer
+              resolution={16}
+              bars={1}
+            >
+              <Sampler
+                sample="samples/kick.wav"
+                steps={[0, 2, 8, 10]}
+              />
+              <Sampler
+                sample="samples/snare.wav"
+                steps={[4, 12, 13, 15]}
+              />
+            </Sequencer>
+            <Sequencer
+              resolution={16}
+              bars={2}
+            >
+              <Polysynth
+                steps={[
+                  [0, 1, ['c3', 'd#3', 'g3' ]],
+                  [2, 1, ['c4']],
+                  [8, 1, ['c3', 'd#3', 'g3']],
+                  [10, 1, ['c4']],
+                  [12, 1, ['c3', 'd#3', 'g3']],
+                  [14, 1, ['d#4']],
+                  [16, 1, ['f3', 'g#3', 'c4']],
+                  [18, 1, ['f3', 'g#3', 'c4']],
+                  [24, 1, ['f3', 'g#3', 'c4']],
+                  [26, 1, ['f3', 'g#3', 'c4']],
+                  [28, 1, ['f3', 'g#3', 'c4']],
+                  [30, 1, ['f3', 'g#3', 'c4']],
+                ]}
+              />
+            </Sequencer>
+            <Sequencer
+              resolution={16}
+              bars={2}
+            >
+              <Synth
+                type="sine"
+                steps={[
+                  [0, 8, 'c2'],
+                  [8, 4, 'c2'],
+                  [12, 4, 'd#2'],
+                  [16, 8, 'f2'],
+                  [24, 8, 'f1'],
+                ]}
+              />
+            </Sequencer>
+          </Analyser>
+        </Song>
+
+        <div style={{ textAlign: 'center' }}>
+          <p style={this.state.lightMode ? {color: 'black'} : {color: 'white'}}>Light Mode</p>
+          <label className="switch">
+            <input type="checkbox" onChange={this.toggleLightMode} checked={this.state.lightMode} />
+            <div className="slider round"></div>
+          </label>
+        </div>
+
+        <Visualization ref={(c) => { this.visualization = c; }} />
+
+        <button
+          className="react-music-button"
+          type="button"
+          onClick={this.handlePlayToggle}
+        >
+          {this.state.playing ? 'Stop' : 'Play'}
+        </button>
       </div>
     );
   }
 }
-
-export default Music;
