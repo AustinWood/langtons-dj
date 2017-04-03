f0868b9f18e546b1e14389fe129c255194f47c7c
Austin Wood <austin@austinkwood.com>
2017-04-03 15:03:04 -0700

    Remove weird hover tweens

--- frontend/components/canvas.jsx
+++ frontend/components/canvas.jsx
@@ -68,21 +68,38 @@ class Canvas extends React.Component {
 
   onMouseOver(e) {
     const gridCoords = this.convertToGridCoords(e);
-    const x = gridCoords.x * this.props.cellSize + this.props.cellSize / 2;
-    const y = gridCoords.y * this.props.cellSize + this.props.cellSize / 2;
+    this.props.hover(gridCoords.x, gridCoords.y);
+    // const x = gridCoords.x * this.props.cellSize + this.props.cellSize / 2;
+    // const y = gridCoords.y * this.props.cellSize + this.props.cellSize / 2;
 
-    const hover = this.refs.hover;
-    const tween = new Konva.Tween({
-        node: hover,
-        duration: 0.15,
-        x: x,
-        y: y
-    });
-    tween.play();
+    // const hover = this.refs.hover;
+    // const tween = new Konva.Tween({
+    //     node: hover,
+    //     duration: 0.15,
+    //     x: x,
+    //     y: y
+    // });
+    // tween.play();
   }
 
   onMouseLeave() {
     this.props.hover(null, null);
+  }
+
+  hover() {
+    if (this.props.hoverPos.x !== null && this.props.hoverPos.y !== null) {
+      return (
+        <Circle
+          ref='hover'
+          x={this.props.hoverPos.x * this.props.cellSize + this.props.cellSize / 2}
+          y={this.props.hoverPos.y * this.props.cellSize + this.props.cellSize / 2}
+          radius={this.props.cellSize * 0.45}
+          fill={null}
+          stroke={'white'}
+          strokeWidth={2} />
+      );
+    }
+    return null;
   }
 
   render() {
@@ -96,20 +113,10 @@ class Canvas extends React.Component {
         onMouseLeave={this.onMouseLeave} >
 
         <Layer>
-
           <Board width={700} height={700} />
           {this.cells()}
           {this.ants()}
-
-          <Circle
-            ref='hover'
-            x={3 * this.props.cellSize + this.props.cellSize / 2}
-            y={4 * this.props.cellSize + this.props.cellSize / 2}
-            radius={this.props.cellSize * 0.45}
-            fill={null}
-            stroke={'white'}
-            strokeWidth={2} />
-
+          {this.hover()}
         </Layer>
 
       </Stage>
