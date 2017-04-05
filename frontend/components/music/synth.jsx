85308636ebb15e38a6e7ae730f55f15df081372a
Austin Wood <austin@austinkwood.com>
2017-04-05 07:27:09 -0700

    Make synth sound good

--- frontend/components/music/synth.jsx
+++ frontend/components/music/synth.jsx
@@ -5,39 +5,59 @@ export default class Synth extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      stepCount: 0
+      stepCount: 0,
+      prevChord: []
     };
-    this.state.synth = new Tone.PolySynth(3, Tone.Synth).toMaster();
-    this.playMusic = this.playMusic.bind(this);
+
+    var synth = new Tone.PolySynth(6, Tone.FMSynth, {
+			"oscillator" : {
+				"type" : "fatsawtooth",
+				"count" : 3,
+				"spread" : 30
+			},
+			"envelope": {
+				"attack": 0.01,
+				"decay": 0.1,
+				"sustain": 0.5,
+				"release": 0.4,
+				"attackCurve" : "exponential"
+			},
+		}).toMaster();
+
+    this.state.synth = synth;
   }
 
   componentDidUpdate() {
     if (this.props.isPlaying && this.state.stepCount !== this.props.stepCount) {
       this.playMusic();
+    } else if (!this.props.isPlaying) {
+      this.state.synth.triggerRelease(this.state.prevChord);
+      this.state.prevChord = [];
     }
   }
 
   playMusic() {
+    this.state.synth.triggerRelease(this.state.prevChord);
     this.state.stepCount = this.props.stepCount;
     let cellStates = this.props.cellStates;
+    console.log(cellStates);
     let chord = [];
     for (let i = 0; i < cellStates.length; i++) {
-      switch (i) {
-        case 0:
-          chord.push(["E4","C3","G3","C4","C4","G3"][cellStates[i]]);
-        case 1:
-          chord.push(["E3","C2","G2","C3","C2","G2"][cellStates[i]]);
-        default:
-          chord.push(["E5","C5","G5","C6","C5","G5"][cellStates[i]]);
-      }
+      const chords = [
+        ["E4","C3","G3","C4","C4","G3"],
+        ["E3","C2","G2","C3","C2","G2"],
+        ["E5","C5","G5","C6","C5","G5"]
+      ];
+      chord.push(chords[i][cellStates[i]]);
     }
-
+    console.log(chord);
     //a polysynth composed of 6 Voices of Synth
     // var synth = new Tone.PolySynth(3, Tone.Synth).toMaster();
     //set the attributes using the set interface
     this.state.synth.set("detune", 0);
     //play a chord
-    this.state.synth.triggerAttackRelease(chord, "8n");
+    this.state.synth.triggerAttackRelease(chord, "1m");
+    this.state.prevChord = chord;
   }
 
   render() {
