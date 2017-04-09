1566f2a4470fd8a79641faaa182132ec630640d8
Austin Wood <austin@austinkwood.com>
2017-04-09 13:36:14 -0700

    Play single note using triggerAttack

--- frontend/components/sequencer.jsx
+++ frontend/components/sequencer.jsx
@@ -20,7 +20,7 @@ export default class Sequencer extends React.Component {
   // SYNTHESIZERS
 
   initializeSynthesizers() {
-    let treble1 = new Tone.FMSynth({
+    const synthProperties = {
       "oscillator" : {
         "type" : "fatsawtooth",
         "count" : 3,
@@ -33,7 +33,9 @@ export default class Sequencer extends React.Component {
         "release": 0.4,
         "attackCurve" : "exponential"
       }
-    }).toMaster();
+    };
+
+    let treble1 = new Tone.FMSynth(synthProperties).toMaster();
 
     this.state.treble1 = treble1;
   }
@@ -44,11 +46,15 @@ export default class Sequencer extends React.Component {
   initializeStepper() {
     this.handleDispatch = this.handleDispatch.bind(this);
     let handleDispatchB = this.handleDispatch;
-    const treble1 = this.state.treble1;
+
+    this.playTreble1 = this.playTreble1 = this.playTreble1.bind(this);
+    let playTreble1B = this.playTreble1;
+
     let stepper = new Tone.Part(function(time, note) {
       handleDispatchB();
-      treble1.triggerAttack("A4");
+      playTreble1B();
     }, []);
+
     stepper.loop = true;
     stepper.loopEnd = "16n";
     stepper.start(0);
@@ -63,35 +69,45 @@ export default class Sequencer extends React.Component {
   // CALCULATE CHORD
 
   componentDidUpdate() {
+    console.log("did update");
     this.toggleTransport();
     this.calculateChord();
   }
 
   calculateChord() {
-    const pentatonic = [
+    console.log("calculate chord");
+    const noteCollection = [
       ["C2", "C3", "C4", "C5", "C6"],
       ["G3", "G4", "G5", "F3", "F4"],
       ["E3", "E4", "E5", "A3", "A4"],
-      ["C2", "C3", "C4", "C5", "C6"]
+      ["C2", "C3", "C4", "C5", "C1"]
     ];
     let newChord = [];
-    let bassNote = [];
-    let altoNote = [];
     const chordObj = this.props.chord;
     let i = 0;
     for (var key in chordObj) {
       if (chordObj.hasOwnProperty(key)) {
-        const pentatonicSub = pentatonic[i];
-        if (pentatonicSub !== null) {
+        const noteArr = noteCollection[i];
+        if (noteArr !== null) {
           const cellState = chordObj[key];
-          const note = pentatonicSub[cellState];
+          const note = noteArr[cellState];
           newChord.push(note);
         }
         i += 1;
       }
     }
+    console.log(newChord);
+    this.state.chord = newChord;
     this.state.stepper.removeAll();
-    this.state.stepper.add(0, newChord);
+    this.state.stepper.add(0, []);
+  }
+
+  /////////////////////////
+  // PLAY CHORD
+
+  playTreble1() {
+    const treble1Note = this.state.chord[3];
+    this.state.treble1.triggerAttack(treble1Note);
   }
 
   /////////////////////////
