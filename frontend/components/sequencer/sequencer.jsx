44a5af80e3e5ce0dc586c93e7c41cb030b7e94bd
Austin Wood <austin@austinkwood.com>
2017-04-11 18:53:33 -0700

    Add more chords

--- frontend/components/sequencer/sequencer.jsx
+++ frontend/components/sequencer/sequencer.jsx
@@ -90,7 +90,7 @@ export default class Sequencer extends React.Component {
 
   calculateChord() {
     console.log("calculate chord");
-    const noteCollection = notes.wholeTone;
+    const noteCollection = notes.happy;
     let newChord = [];
     const chordObj = this.props.chord;
     let i = 0;
